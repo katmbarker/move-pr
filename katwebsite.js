c2f52dc01db0a59f4b6bbdc02522c2572ea06f21
Kat Barker <katmeadbarker@gmail.com>
2017-03-29 18:18:08 -0400

    second commit

--- katwebsite.js
+++ katwebsite.js
@@ -1,34 +1,36 @@
-function formatDate(date) {
-  var hours = date.getHours();
-  var minutes = date.getMinutes();
-  var ampm = hours >= 12 ? 'pm' : 'am';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0'+minutes : minutes;
-  var strTime = hours + ':' + minutes + ' ' + ampm;
-  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
-}
-var d = new Date();
-var e = formatDate(d);
-
-$('#overlay').html('<h1>' + d + '</h1>');
-
-
-(document).ready(function($) {
+$(document).ready(function() {
 	console.log('hello');
   $.ajax({
-  url : "http://api.wunderground.com/api/95f3ed1830df06dd/geolookup/conditions/q/PR/San_Juan.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  var precipitation = parsed_json['precipitation'];
-  $("#overlay").html('<h1>' + location + temp_f + precipitation + '</h1>');
-   // if (temp_f > 75) {
-   //      function show_image(src) {
-   //      var img = document.createElement("#sun_icon");
-   //      img.src = "sun_icon.png";
+    url : "http://api.wunderground.com/api/95f3ed1830df06dd/geolookup/conditions/q/PR/San_Juan.json",
+    dataType : "jsonp",
+    success : function(parsed_json) {
+    var location = parsed_json['location']['city'];
+    var temp_f = parsed_json['current_observation']['temp_f'];
+    var precipitation = parsed_json['current_observation']['weather'];
+    var icon = parsed_json['current_observation']['icon_url'];
+    console.log(parsed_json);
+    var d = new Date();
+    var e = formatDate(d);
 
-   }
+    $('#overlay').append('<h1 class="overlay">' + e + '</h1>');
+    $("#overlay").append('<h1 class="overlay">' + ' ' + location + ' ' + temp_f + '</h1>');
+    $("#overlay").append('<h1 class="overlay">' + precipitation + ' ' + '</h1>');
+    $("#overlay").append('<img class="dailyreport" src="' + icon + '" />');
+
+     }
+   
+  });
+    function formatDate(date) {
+      var hours = date.getHours();
+      var minutes = date.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+      var strTime = hours + ':' + minutes + ' ' + ampm;
+      return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+    }
+
 });
+
 
